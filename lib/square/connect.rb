0cb894c9516ea4af52a85b0ec0cd38f4847fb94f
Conroy Whitney <conroy.whitney@gmail.com>
2015-12-31 01:44:57 -0500

    Initial gem creation using bundler

---
+++ lib/square/connect.rb
@@ -0,0 +1,7 @@
+require "square/connect/version"
+
+module Square
+  module Connect
+    # Your code goes here...
+  end
+end
---
+++ spec/square/connect_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Square::Connect do
+  it 'has a version number' do
+    expect(Square::Connect::VERSION).not_to be nil
+  end
+end
