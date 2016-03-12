472fce09cbb08f949e69188218dd8ee16ff13717
Conroy Whitney <conroy.whitney@gmail.com>
2016-03-12 13:48:33 -0800

    Ensure Item matches API docs
    Ensure all expected attributes are present on
    Square::Connect::Resource::Item that are listed in the API docs at
    https://docs.connect.squareup.com/api/connect/v1/#datatype-item. Set
    nils for now for things not supported (e.g., fees, modifier_lists).

--- lib/square/connect/resources/item.rb
+++ lib/square/connect/resources/item.rb
@@ -4,34 +4,63 @@ module Square
       class Item
         # https://docs.connect.squareup.com/api/connect/v1/#datatype-item
 
-        attr_reader :id, :fees, :variations, :available_for_pickup, :available_online, :visibility,
-                     :description, :name, :category_id, :category, :type
+        attr_reader \
+          :id,
+          :name,
+          :description,
+          :type,
+          :abbreviation,
+          :color,
+          :visibility,
+          :available_online,
+          :available_for_pickup,
+          :master_image,
+          :category_id,
+          :category,
+          :variations,
+          :modifier_lists,
+          :fees
 
         def initialize(item)
-          puts item
           raise ArgumentError, 'Error parsing Item from nil' unless item
 
           @id = item['id']
-          @fees = item['fees']
-          @variations = item['variations'].map(&method(:parse_variation)).compact
-          @available_for_pickup = item['available_for_pickup']
-          @available_online = item['available_online']
-          @visibility = item['visibility']
-          @description = item['description']
           @name = item['name']
-          @category = parse_category(item['category'])
-          @category_id = item['category_id']
+          @description = item['description']
           @type = item['type']
+          @abbreviation = item['abbreviation']
+          @color = item['color']
+          @visibility = item['visibility']
+          @available_online = item['available_online']
+          @available_for_pickup = item['available_for_pickup']
+          @master_image = item['master_image']
+          @category_id = item['category_id']
+          @category = parse_category(item['category'])
+          @variations = parse_variations(item['variations'])
+          @modifier_lists = parse_modifier_lists(item['modifier_lists'])
+          @fees = parse_fees(item['fees'])
         end
 
         private
+
+        def parse_category(category)
+          category ? Square::Connect::Resource::Category.new(category) : nil
+        end
+
+        def parse_variations(variations)
+          variations.map(&method(:parse_variation)).compact
+        end
 
         def parse_variation(variation)
           variation ? Square::Connect::Resource::ItemVariation.new(variation) : nil
         end
 
-        def parse_category(category)
-          category ? Square::Connect::Resource::Category.new(category) : nil
+        def parse_modifier_lists(modifier_lists)
+          nil
+        end
+
+        def parse_fees(fees)
+          nil
         end
       end
     end
--- spec/square/connect/resources/item_spec.rb
+++ spec/square/connect/resources/item_spec.rb
@@ -1,37 +1,45 @@
 RSpec.describe Square::Connect::Resource::Item do
-  subject(:item)   { described_class.new(response_item) }
-  let(:variations) { ['ITEM VARIATION 1', 'ITEM VARIATION 2'] }
-  let(:category)   { 'ITEM CATEGORY' }
+  subject(:item)     { described_class.new(response_item) }
+
+  let(:master_image) { 'ITEM MASTER IMAGE' }
+  let(:variations)   { ['ITEM VARIATION 1', 'ITEM VARIATION 2'] }
+  let(:category)     { 'ITEM CATEGORY' }
   let(:response_item) do
     {
       'id' => 'ITEM ID',
-      'fees' => ['ITEM FEES'],
-      'variations' => variations,
-      'available_for_pickup' => 'ITEM PICKUP',
-      'available_online' => 'ITEM ONLINE',
-      'visibility' => 'ITEM VISIBILITY',
-      'description' => 'ITEM DESCRIPTION',
       'name' => 'ITEM NAME',
+      'description' => 'ITEM DESCRIPTION',
+      'type' => 'ITEM TYPE',
+      'abbreviation' => 'ITEM ABBREVIATION',
+      'color' => 'ITEM COLOR',
+      'visibility' => 'ITEM VISIBILITY',
+      'available_online' => 'ITEM ONLINE',
+      'available_for_pickup' => 'ITEM PICKUP',
+      'master_image' => master_image,
       'category_id' => 'ITEM CATEGORY ID',
       'category' => category,
-      'type' => 'ITEM TYPE'
+      'variations' => variations,
+      'modifier_lists' => ['ITEM MODIFIER LIST 1', 'ITEM MODIFIER LIST 2'],
+      'fees' => ['ITEM FEE 1', 'ITEM FEE 2']
     }
   end
 
   describe 'initialize' do
     it { expect(item.id).to eq 'ITEM ID' }
-    it { expect(item.fees).to be_an(Array) }
-    it { expect(item.fees.first).to eq 'ITEM FEES' }
-    it { expect(item.variations).to be_an(Array) }
-    it { expect(item.variations.first).to be_a(Square::Connect::Resource::ItemVariation) }
-    it { expect(item.available_for_pickup).to eq 'ITEM PICKUP' }
-    it { expect(item.available_online).to eq 'ITEM ONLINE' }
-    it { expect(item.visibility).to eq 'ITEM VISIBILITY' }
-    it { expect(item.description).to eq 'ITEM DESCRIPTION' }
     it { expect(item.name).to eq 'ITEM NAME' }
+    it { expect(item.description).to eq 'ITEM DESCRIPTION' }
+    it { expect(item.type).to eq 'ITEM TYPE' }
+    it { expect(item.abbreviation).to eq 'ITEM ABBREVIATION' }
+    it { expect(item.color).to eq 'ITEM COLOR' }
+    it { expect(item.visibility).to eq 'ITEM VISIBILITY' }
+    it { expect(item.available_online).to eq 'ITEM ONLINE' }
+    it { expect(item.available_for_pickup).to eq 'ITEM PICKUP' }
+    it { expect(item.master_image).to eq 'ITEM MASTER IMAGE' }
     it { expect(item.category_id).to eq 'ITEM CATEGORY ID' }
     it { expect(item.category).to be_a(Square::Connect::Resource::Category) }
-    it { expect(item.type).to eq 'ITEM TYPE' }
+    it { expect(item.variations).to be_an(Array) }
+    it { expect(item.variations.first).to be_a(Square::Connect::Resource::ItemVariation) }
+    it { expect(item.fees).to eq nil }
 
     context 'when response_item is nil' do
       let(:response_item) { nil }
